61a9abbed4d71e43642c24e6b23de91d4ae4b17d
8210182092 <92192693+8210182092@users.noreply.github.com>
2024-01-19 00:32:13 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Abhishek Pandey',
+    author_email= 'harekrishna112ap@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
